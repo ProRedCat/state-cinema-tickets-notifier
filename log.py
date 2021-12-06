24e6fba2433f3b18c1e5afb9c616b52e35802e3d
ProRedCat <43058406+ProRedCat@users.noreply.github.com>
2021-12-06 19:07:33 +1300

    Added comments and docs

--- log.py
+++ log.py
@@ -3,23 +3,30 @@ import os.path
 from checkmovies import *
 
 def logTitle(title):
+    """Method to log the title into a file called movies.txt"""
     file = open("movies.txt", "a")
     file.write(title + "\n")
     file.close()
 
 def loadTitles():
+    """Method to load the titles on program start"""
     titles = set()
 
+    # If the movies.txt file does exist (program has already been run before)
     if(os.path.exists("movies.txt")):
+        # Create movies.txt and start writing to it 
         file = open("movies.txt", "r")
 
+        # Get each title and remove the \n on the end and add to titles set
         for title in file:
             titles.add(title.removesuffix("\n"))
 
         file.close()
-    else:
+    else: # Else this is the first time start up 
+        # Get all current movies showing
         titles = getMovieTitles()
 
+        # Log these titles
         for title in titles:
             logTitle(title)
 
--- notify.py
+++ notify.py
@@ -9,19 +9,26 @@ SMTP_ADDRESS = "xxx"
 RECIPIENT = "xxx"
 
 def connectToEmailServer():
+    """Method to connect to an email server and log in"""
     context = ssl.create_default_context()
 
+    #Connect to the SMTP server
     server = smtplib.SMTP_SSL(SMTP_ADDRESS, PORT, context=context)
+
+    #Log into the server
     server.login(USERNAME, PASSWORD)
 
     return server
 
 def disconnectEmailServer(server):
+    """Method to disconnect from the email server"""
     server.quit()
 
 # Email formal \Subject: {subject} {body}
 def sendEmail(server, message):
+    """Method to send a email given a server and the message to send"""
     server.sendmail(USERNAME, RECIPIENT, message)
 
 def createEmail(subject, body):
+    """Method to create the string format for the email"""
     return "Subject: {}\n\n{}".format(subject, body)
